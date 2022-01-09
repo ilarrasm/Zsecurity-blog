82939f68202b75ecee8dcc632ef9b9fa0162d977
Martin ilarras <martin.ilarras1@gmail.com>
2022-01-09 01:53:39 -0300

    1:53

--- script/app.js
+++ script/app.js
@@ -2,3 +2,41 @@ function hamburguesa() {
   var element = document.getElementById("menu");
   element.classList.toggle("menuRes");
 } 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "25%";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "flex") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "flex";
+  }
+  });
+}
+function userDrop() {
+  document.getElementById("userDate").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("user-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
