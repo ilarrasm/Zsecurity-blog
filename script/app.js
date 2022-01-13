2da2893132f0ca5f7a2808275f6f2e3ae674f91c
Martin ilarras <martin.ilarras1@gmail.com>
2022-01-13 15:26:01 -0300

    15:25

--- script/app.js
+++ script/app.js
@@ -1,6 +1,10 @@
 function hamburguesa() {
-  var element = document.getElementById("menu");
-  element.classList.toggle("menuRes");
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 } 
 function openNav() {
   document.getElementById("mySidenav").style.width = "22%";
